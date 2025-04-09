202338cb4cc3c2e23d8d4c49a71eb880c4e0d8d1
Vibha Pathak <vibha4554@gmail.com>
2025-04-10 00:12:51 +0530

    Deleted unused folders and added new dist files

--- client/src/pages/chat/components/empty-chat-container/index.jsx
+++ client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const EmptyChatContainer = () => {
   return (
-    <div className='flex-1 bg-[#FFDFEF] md:flex flex-col justify-center items-center hidden duration-1000 transition-all'>
+    <div className='flex-1 bg-[rgb(250,217,233)] md:flex flex-col justify-center items-center hidden duration-1000 transition-all'>
       <video
         src="/culture.mp4"
         autoPlay
         muted
         loop
         playsInline
-        className="h-full w-auto max-h-full rounded-2xl shadow-xl object-contain"
+        className="h-full w-auto max-h-full object-contain"
       />
     </div>
   );
