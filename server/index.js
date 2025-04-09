a44a06e134f1bfe4cc11e158b08d15c4450bdd46
Vibha Pathak <vibha4554@gmail.com>
2025-04-10 00:40:29 +0530

    Fix static path to frontend build

--- server/index.js
+++ server/index.js
@@ -51,10 +51,13 @@ app.use("/api/comment", commentRoutes);
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
 // Catch-all route for React (SPA)
-app.get("*", (req, res, next) => {
-  if (req.path.startsWith("/storyboard/")) return next(); 
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
 });
+
 
 // Start server and connect DB
 const server = app.listen(port, () => {
